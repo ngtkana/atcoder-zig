1383417d48b937ce743eaa570f33ed6512cfac63
Kana Nagata <ngtkana@gmail.com>
2026-06-18 18:46:19 +0900

    libio

--- src/main.zig
+++ src/main.zig
@@ -1,41 +1,50 @@
 const std = @import("std");
+const nextInt = libio.nextInt;
+const print = libio.print;
 
 pub fn main() !void {
-    // stdin から全入力を読み込む
+    try libio.init();
+    defer libio.deinit();
+
+    const x = try nextInt(usize);
+    const ans = switch (3 <= x and x <= 18) {
+        true => "Yes",
+        false => "No",
+    };
+    try print("{s}", .{ans});
+}
+
+// {{{ libio: 標準入出力のコーナーでございます👆️
+const libio = struct {
     const stdin = std.fs.File.stdin();
-    var input_buf: [1024 * 1024 * 10]u8 = undefined;
-    const bytes_read = try stdin.readAll(&input_buf);
-    const input_data = input_buf[0..bytes_read];
-
-    // 入力を空白文字で分割
-    var tokens = std.mem.tokenizeAny(u8, input_data, " \t\r\n");
-
-    // stdout へのライター
     const stdout = std.fs.File.stdout();
 
-    // --- ここから問題の解法記述 ---
+    var _stdin_buf: [1024]u8 = undefined;
+    var stdin_reader = stdin.reader(&_stdin_buf);
 
-    // 例1: 整数Nの入力
-    const n_str = tokens.next().?;
-    const n = try std.fmt.parseInt(i32, n_str, 10);
-
-    // 例2: 長さNの配列の入力と出力
-    var output_buf: [65536]u8 = undefined;
-    var output_pos: usize = 0;
-
-    var written = try std.fmt.bufPrint(output_buf[output_pos..], "N is: {d}\n", .{n});
-    output_pos += written.len;
-
-    var i: i32 = 0;
-    while (i < n) : (i += 1) {
-        if (tokens.next()) |token| {
-            const val = try std.fmt.parseInt(i32, token, 10);
-            written = try std.fmt.bufPrint(output_buf[output_pos..], "{d} ", .{val * 2});
-            output_pos += written.len;
-        }
+    var _stdout_buf: [4096]u8 = undefined;
+    fn print(comptime fmt: []const u8, args: anytype) !void {
+        const output = try std.fmt.bufPrint(&_stdout_buf, fmt, args);
+        try stdout.writeAll(output);
     }
-    written = try std.fmt.bufPrint(output_buf[output_pos..], "\n", .{});
-    output_pos += written.len;
 
-    try stdout.writeAll(output_buf[0..output_pos]);
-}
+    var arena: std.heap.ArenaAllocator = undefined;
+    var allocator: std.mem.Allocator = undefined;
+    var input_tokens: std.mem.TokenIterator(u8, .any) = undefined;
+
+    fn nextInt(comptime T: type) !T {
+        return std.fmt.parseInt(T, input_tokens.next().?, 10);
+    }
+
+    fn init() !void {
+        arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+        allocator = arena.allocator();
+        const input_data = try stdin_reader.interface.allocRemaining(allocator, .unlimited);
+        input_tokens = std.mem.tokenizeAny(u8, input_data, " \t\r\n");
+    }
+
+    fn deinit() void {
+        arena.deinit();
+    }
+};
+// }}}
