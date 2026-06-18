365594497e0cffd5e7a0dda8bb2deae158ade073
Kana Nagata <ngtkana@gmail.com>
2026-06-18 21:44:57 +0900

    add libfmt

--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,99 @@
 const std = @import("std");
 const nextInt = libio.nextInt;
 const print = libio.print;
+const dbg = libfmt.dbg;
+
+const Item = struct {
+    c: usize,
+    v: u64,
+};
 
 pub fn main() !void {
     try libio.init();
     defer libio.deinit();
 
-    const x = try nextInt(usize);
-    const ans = switch (3 <= x and x <= 18) {
-        true => "Yes",
-        false => "No",
-    };
-    try print("{s}", .{ans});
+    const n = try nextInt(usize);
+    const k = try nextInt(usize);
+    const m = try nextInt(usize);
+
+    const a = try libio.alloc.alloc(u64, 2 * n);
+    @memset(a, 0);
+    for (0..n) |i| {
+        const c = try nextInt(usize) - 1;
+        var v = try nextInt(u64);
+        if (a[c] < v) {
+            std.mem.swap(u64, &a[c], &v);
+        }
+        a[n + i] = v;
+    }
+    std.mem.sortUnstable(u64, a[0..n], {}, std.sort.desc(u64));
+    std.mem.sortUnstable(u64, a[m .. 2 * n], {}, std.sort.desc(u64));
+
+    var ans: u64 = 0;
+    for (a[0..k]) |item| {
+        ans += item;
+    }
+    try print("{d}", .{ans});
 }
 
+// {{{ libfmt: デバッグ出力のフォーマットでございます👆️
+const libfmt = struct {
+    fn dbg(value: anytype) void {
+        std.debug.print("{f}\n", .{fmtArray(value)});
+    }
+
+    fn fmtArray(value: anytype) FmtArray(@TypeOf(value)) {
+        return .{ .value = value };
+    }
+
+    fn fmtByFor(value: anytype, writer: anytype) !void {
+        try writer.writeAll("[");
+        var is_head = true;
+        for (value) |item| {
+            if (is_head) {
+                is_head = false;
+            } else {
+                try writer.writeAll(", ");
+            }
+            try writer.print("{f}", .{fmtArray(item)});
+        }
+        try writer.writeAll("]");
+    }
+
+    fn FmtArray(comptime T: type) type {
+        return struct {
+            value: T,
+
+            pub fn format(
+                self: @This(),
+                writer: anytype,
+            ) !void {
+                const value = self.value;
+                const type_info = @typeInfo(T);
+                switch (type_info) {
+                    .pointer => {
+                        switch (type_info.pointer.size) {
+                            .slice => return try fmtByFor(value, writer),
+                            else => {},
+                        }
+                    },
+                    .@"struct" => {
+                        inline for (type_info.@"struct".fields) |field| {
+                            if (std.mem.eql(u8, field.name, "items") and
+                                @typeInfo(field.type) == .pointer and @typeInfo(field.type).pointer.size == .slice)
+                            {
+                                return try fmtByFor(value.items, writer);
+                            }
+                        }
+                    },
+                    else => {},
+                }
+                try writer.print("{any}", .{value});
+            }
+        };
+    }
+};
+// }}}
 // {{{ libio: 標準入出力のコーナーでございます👆️
 const libio = struct {
     const stdin = std.fs.File.stdin();
@@ -29,7 +109,7 @@ const libio = struct {
     }
 
     var arena: std.heap.ArenaAllocator = undefined;
-    var allocator: std.mem.Allocator = undefined;
+    var alloc: std.mem.Allocator = undefined;
     var input_tokens: std.mem.TokenIterator(u8, .any) = undefined;
 
     fn nextInt(comptime T: type) !T {
@@ -38,8 +118,8 @@ const libio = struct {
 
     fn init() !void {
         arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-        allocator = arena.allocator();
-        const input_data = try stdin_reader.interface.allocRemaining(allocator, .unlimited);
+        alloc = arena.allocator();
+        const input_data = try stdin_reader.interface.allocRemaining(alloc, .unlimited);
         input_tokens = std.mem.tokenizeAny(u8, input_data, " \t\r\n");
     }
 
